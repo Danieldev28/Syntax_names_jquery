844c5963783bf96a7a2ef234fe0ccd725e1a8f56
Ubuntu <ubuntu@ip-172-31-80-200.ec2.internal>
2019-06-21 17:04:13 +0000

    historic file learning jquery

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// $("myButton").on("click", function() {
+// $(".any2").first().css("border","5px solid black");
+// })
+ $("myButton").on("click",function(){
+    $("#myPara").first().css();
+  });
